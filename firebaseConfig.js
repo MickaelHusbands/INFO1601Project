56675a171475fbd39b4163a5d62177e8bc12e776
MickaelHusbands <mickaelhusbands63@gmail.com>
2026-04-12 20:29:01 -0400

    1st Commit

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAbxzBoS1L3Hzu91ONszyr8lO3raHc8PPo",
+  authDomain: "info1601project-d6de8.firebaseapp.com",
+  projectId: "info1601project-d6de8",
+  storageBucket: "info1601project-d6de8.firebasestorage.app",
+  messagingSenderId: "665943691969",
+  appId: "1:665943691969:web:162518980ccf78bf15fa40"
+};
+
+export default firebaseConfig;
